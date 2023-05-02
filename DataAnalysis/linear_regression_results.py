06fad8d0fcd8c9842cf119e5a1db24670597d494
MadCodes9 <ceiaram@cpp.edu>
2023-05-02 15:00:00 -0700

    Modified signifigance column to display if independent variable has any effect

--- DataAnalysis/linear_regression_results.py
+++ DataAnalysis/linear_regression_results.py
@@ -1,3 +1,8 @@
+# can be answered with regression of the individual consumer review --- 
+# IV = the number of mentions of the indicative keywords of an online review, plus start rating, number of words, etc.
+#  (see some variable used in my other paper attached). DV = helpfulness votes an online review received. 
+#  (we can also use the mentions of sustainability keywords to predict “value” besides helpfulness votes)
+
 import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
@@ -6,8 +11,16 @@ from sklearn.metrics import mean_squared_error, r2_score
 import xlsxwriter
 import matplotlib.pyplot as plt
 
+
+
 # Load the dataset
 df = pd.read_excel('linear_regression_data.xlsx')
+
+# Winsorize the data to remove extreme outliers
+lower = df.quantile(0.05)
+upper = df.quantile(0.95)
+df = df.apply(lambda x: np.clip(x, lower[x.name], upper[x.name]))
+
 
 # Split the data into training and testing sets
 X = df[['Customer Rating', 'Number of Words in Review', 'Number of Sentences in Review', 'Manager Response', 'Sustainabilty Score', 
@@ -52,11 +65,16 @@ worksheet = workbook.add_worksheet()
 worksheet.write('A1', 'Independent Variable')
 worksheet.write('B1', 'Coefficient')
 worksheet.write('C1', 'Significance')
+# Coefficient is close to zero, we can set a threshold value and check if the absolute value of the coefficient is below that threshold.
 for i, (name, coef) in enumerate(zip(X.columns, model.coef_)):
-    significance = 'significant' if coef != 0 else ''
+    # If it is below the threshold, we can consider the independent variable to have little or no effect on the dependent variable.
+    if abs(coef) < 0.1:
+        effect = 'Little or no effect'
+    else:
+        effect = 'Significant predictor'
     worksheet.write(i+1, 0, name)
     worksheet.write(i+1, 1, coef)
-    worksheet.write(i+1, 2, significance)
+    worksheet.write(i+1, 2, effect)
 
 worksheet.write('A8', 'MSE')
 worksheet.write('B8', mse)
@@ -67,11 +85,16 @@ workbook.close()
 
 
 # Plot the actual vs. predicted values
-plt.scatter(y_test, y_pred)
+plt.scatter(y_test, y_pred, alpha=0.5) # Alpha adjusts transparency of points
 plt.plot(y_test, y_test, 'r-')
 plt.xlabel('Actual Helpful Votes')
 plt.ylabel('Predicted Helpful Votes')
 plt.title('Linear Regression Model')
+
+# # # Set the axis limits to increase the scale
+# # plt.xlim(0, 0.001)
+# # plt.ylim(0, 0.001)
+
 
 # add text to the plot
 plt.text(50, 200, f'MSE = {mse:.2f}\nR-squared = {r2:.2f}', fontsize=12)
@@ -80,7 +103,6 @@ plt.legend(['Predicted Values', 'Actual Values'], loc='upper left')
 # Save graph as .png
 plt.savefig('regression_plot.png')
 plt.show()
-
 
 # # Make predictions on new data
 # new_data = pd.DataFrame({'Customer Rating': [1, 2, 3], 'Number of Words in Review': [4, 5, 6], 'Number of Sentences in Review': [7, 8, 9], 
